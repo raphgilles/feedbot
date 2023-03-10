43e2228c64437e42feb7ecd52fcb2fa687a719b1
StefOfficiel <stephane@s-pichard.fr>
2023-03-10 18:13:12 +0100

    Modifications mineures
    Réindentation. Manque-t-il des fermetures de div ou sont-elles dans un autre fichier ?

--- global.php
+++ global.php
@@ -13,63 +13,65 @@ $totalposts = mysqli_num_rows($result);
 ?>
 
 <div class="contenair-home">
-	
-<div class="title"><i class="fa fa-globe-w" aria-hidden="true"></i> <?= GLOBAL_FEED ?></div>
-
-<div class="story_contenair">	
+	<div class="title">
+		<i class="fa fa-globe-w" aria-hidden="true"></i> <?= GLOBAL_FEED ?>
+	</div>
+	<div class="story_contenair">
 <?php include('./template-parts/widget-stories.php'); ?>
-</div>
-
-<div style="clear:both;">
-
-<div class="widget-home">
+	</div>
+	<div style="clear:both;">
+		<div class="widget-home">
 <?php include('./template-parts/widget-suggestions.php'); ?>
 <?php include('./template-parts/widget-shares.php'); ?>
 <?php include('./template-parts/widget-funding.php'); ?>
-<?php include('template-parts/footer.php'); ?>
-</div>
-
-<div>
+<?php include('./template-parts/footer.php'); ?>
+		</div>
+		<div>
 
 <?php
 $actualtime = time();
 $sql = "SELECT * FROM articles WHERE date > '$lastid' AND date < $actualtime ORDER BY date DESC LIMIT 10";
 $result = mysqli_query($conn, $sql);
 include('./template-parts/global-timeline.php');
-if ($isconnected !== NULL) { include('./template-parts/autopromo.php'); }
+if($isconnected !== NULL){
+	include('./template-parts/autopromo.php');
+}
 ?>
 
-<?php if ($isconnected == "") {
-include('./template-parts/not-connected.php');
-} else { ?>
-<div id="post-data" class="appear"></div>
-
-</div>
-<script>
-var isActive = false;
-$(window).scroll(function() {
-    if(!isActive && $(window).scrollTop() + $(window).height() >= $(document).height() - 550) {
-        isActive = true;
-        var last_id = $(".content-home:last").attr("id");
-        loadMore(last_id);
-    }
-});
-
-function loadMore(last_id){
-  var website = window.location.origin;
-  $.ajax({
-      url: website + '/includes/infinite-global.php?last_id=' + last_id,
-      type: "GET",
-      beforeSend: function(){
-          $('.ajax-load').show();
-      }
-  }).done(function(data){
-      $('.ajax-load').hide();
-      $("#post-data").append(data);
-      isActive = false;
-  }).fail(function(jqXHR, ajaxOptions, thrownError){
-
-  });
+<?php
+if($isconnected == ""){
+	include('./template-parts/not-connected.php');
 }
-</script>
+else{
+?>
+			<div id="post-data" class="appear"></div>
+		</div>
+		<script>
+			var isActive = false;
+			$(window).scroll(function(){
+				if(!isActive && $(window).scrollTop() + $(window).height() >= $(document).height() - 550){
+					isActive = true;
+					var last_id = $(".content-home:last").attr("id");
+					loadMore(last_id);
+				}
+			});
+
+			function loadMore(last_id){
+				var website = window.location.origin;
+				$.ajax({
+					url: website + '/includes/infinite-global.php?last_id=' + last_id,
+					type: "GET",
+					beforeSend: function(){
+						$('.ajax-load').show();
+					}
+				}).done(function(data){
+					$('.ajax-load').hide();
+					$("#post-data").append(data);
+					isActive = false;
+				}).fail(function(jqXHR, ajaxOptions, thrownError){
+
+				});
+			}
+		</script>
 <?php } ?>
+<?php // Manque potentiellement deux fermetures de div // ?>
