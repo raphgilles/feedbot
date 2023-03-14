8e8f0fb8267f0fedf7534a9b503dcb388d12897c
Raph <49836486+raphgilles@users.noreply.github.com>
2023-03-14 08:27:33 +0100

    Add files via upload
    Ajoute de la date de la dernière connexion et de la date d'inscription des utilisateurs

--- includes/add_user_to_db.php
+++ includes/add_user_to_db.php
@@ -1,6 +1,6 @@
 <?php
 include('../config.php');
-
+$current_date = time();
 
 // On vérifie si l'utilisateur existe dans la base de donnée
 $user_query = mysqli_fetch_array(mysqli_query($conn, "SELECT username FROM users WHERE username = '$akainstance'"));
@@ -8,7 +8,7 @@ $user_db_username = $user_query['username'];
 
 // Si l'utilisateur n'existe pas, on l'ajoute 
 if($user_db_username == "") {
-$sql = "INSERT INTO users (username) values ('$akainstance')";
+	$sql = "INSERT INTO users (username) values ('$akainstance')";
 	mysqli_query($conn, $sql);
 	$id_user_query =  mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM users ORDER BY id DESC LIMIT 1"));
 	$id_user = $id_user_query['id'];
@@ -16,11 +16,22 @@ $sql = "INSERT INTO users (username) values ('$akainstance')";
 		$sql = "UPDATE users SET admin = ('1') WHERE id = '1'";
 		mysqli_query($conn, $sql);
 	}
+
+	$sql = "UPDATE users SET joined_date = '$current_date' WHERE id = '$id_user'";
+	mysqli_query($conn, $sql);
+
+	$sql = "UPDATE users SET last_activity = '$current_date' WHERE id = '$id_user'";
+	mysqli_query($conn, $sql);
 }
 
 // Si l'utilisateur existe, on récupère son id
 else {
-	$id_user_query =  mysqli_fetch_array(mysqli_query($conn, "SELECT id FROM users WHERE username = '$akainstance'"));
-	$id_user = $id_user_query['id'];
+	$sql = "SELECT * FROM users WHERE username = '$akainstance'";
+	$result = mysqli_query($conn, $sql);
+	foreach($result as $row) {
+		$id_user = $row['id'];
+	}
+	$sql = "UPDATE users SET last_activity = '$current_date' WHERE username = '$akainstance'";
+	mysqli_query($conn, $sql);
 }
 ?>
