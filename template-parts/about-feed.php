83d9135f4e6015574ae959f35abf9824c6771ddb
StefOfficiel <stephane@s-pichard.fr>
2023-03-16 03:34:22 +0100

    Réindentation + améliorations
    Réindentation de ces 5 fichiers + amélioration d'une str_replace à la ligne 32 de global-timeline.php + retrait des variables inutiles dans autopromo.php

--- template-parts/about-feed.php
+++ template-parts/about-feed.php
@@ -1,37 +1,47 @@
 <?php
+
 $wiki_feed_name = str_replace(" ", '_', $feed_name);
 $wiki_feed_name = urlencode($wiki_feed_name);
-if ($wiki_slug == NULL) {
-$about = getWIKI($lang, $wiki_feed_name);
+
+if($wiki_slug == NULL){
+	$about = getWIKI($lang, $wiki_feed_name);
 }
-else {
-$about = getWIKI($lang, $wiki_slug);
+else{
+	$about = getWIKI($lang, $wiki_slug);
 }
-if ($about !== NULL && $about !== "") {
-$caracters_counter = strlen($about);
+
+if($about !== NULL && $about !== ""){
+	$caracters_counter = strlen($about);
 ?>
 
 <div class="widget-home-content about" style="margin-bottom:20px;">
-<h4 style="color:var(--feedbot-title);"><i class="fa fa-info-circle" aria-hidden="true" style="margin-right:6px;"></i> <?=ABOUT;?></h4>
-<div <?php if ($caracters_counter > "400") { ?>class="description-short"<?php } ?> style="margin-top:20px;" id="about">
-    <span style="word-break: break-word; white-space: pre-line;"><?=$about;?></span> 
-</div>
-<?php if ($caracters_counter > "400") { ?><div id="read-more-about" style="width:100%; font-weight:bold; text-align:center; margin-top:10px; cursor:pointer;"><?= READ_MORE ?></div><?php } ?>
-    <div style="margin-top:20px;">
-        <p style="text-align:right;">Source : <a href="https://<?=$lang;?>.wikipedia.org/wiki/<?=$wiki_feed_name;?>" target="_blank">Wikipedia</a></p>
-    </div>
-
+	<h4 style="color:var(--feedbot-title);">
+		<i class="fa fa-info-circle" aria-hidden="true" style="margin-right:6px;"></i> <?=ABOUT;?>
+	</h4>
+	<div <?php if ($caracters_counter > "400") { ?>class="description-short"<?php } ?> style="margin-top:20px;" id="about">
+		<span style="word-break: break-word; white-space: pre-line;"><?=$about;?></span>
+	</div>
+<?php if ($caracters_counter > "400") { ?>
+	<div id="read-more-about" style="width:100%; font-weight:bold; text-align:center; margin-top:10px; cursor:pointer;">
+		<?=READ_MORE;?>
+	</div>
+<?php } ?>
+	<div style="margin-top:20px;">
+		<p style="text-align:right;">Source : <a href="https://<?=$lang;?>.wikipedia.org/wiki/<?=$wiki_feed_name;?>" target="_blank">Wikipedia</a></p>
+	</div>
 </div>
 
 <?php if ($caracters_counter > "400") { ?>
 <script type="text/javascript">
-    function expand_about() {
-    document.getElementById('about').classList.remove('description-short');
-    document.getElementById('read-more-about').style.display = 'none';
-    }
-    document.getElementById('read-more-about').addEventListener('click', expand_about);
+	function expand_about(){
+		document.getElementById('about').classList.remove('description-short');
+		document.getElementById('read-more-about').style.display = 'none';
+	}
+
+	document.getElementById('read-more-about').addEventListener('click', expand_about);
+	// Est-ce que le jQuery est appelé avant ? // Si oui, à écrire en jQuery
 </script>
 <?php
+	}
 }
-
-} ?>
+?>
--- template-parts/autopromo.php
+++ template-parts/autopromo.php
@@ -1,34 +1,36 @@
 <div class="autopromo" style="padding-top: 15px; padding-bottom: 25px;">
-    <div style="height:75px;">
+	<div style="height:75px;">
+		<div style="width: 60px; aspect-ratio: 1 / 1; background-image: url(<?=WEBSITE_URL;?>/assets/icons/icon.png); background-size: cover; background-position: center; border-radius: 50%; float:left; overflow: hidden;">
+			<a href="<?=WEBSITE_URL;?>" style="display:block; width:100%; height:100%;"></a>
+		</div>
+		<div style="margin-left: 74px; padding-top: 13px; line-height: 16px;">
+			<a href="<?=WEBSITE_URL;?>" style="color:var(--feedbot-title); font-weight: bold; text-decoration: none;"><?= WEBSITE_NAME; ?></a>
+			<br>
+			<span style="font-size: 12px; color:var(--feedbot-text);"><?php echo relativedate(time()); ?></span>
+		</div>
+	</div>
 
-        <div style="width: 60px; aspect-ratio: 1 / 1; background-image: url(<?=WEBSITE_URL;?>/assets/icons/icon.png); background-size: cover; background-position: center; border-radius: 50%; float:left; overflow: hidden;"><a href="<?=WEBSITE_URL;?>" style="display:block; width:100%; height:100%;"></a></div>
-        <div style="margin-left: 74px; padding-top: 13px; line-height: 16px;"><a href="<?=WEBSITE_URL;?>" style="color:var(--feedbot-title); font-weight: bold; text-decoration: none;"><?= WEBSITE_NAME; ?></a><br />
-        <span style="font-size: 12px; color:var(--feedbot-text);"><?php echo relativedate(time()); ?></span>
-        </div>
-    </div>
+	<div style="margin-bottom: 20px; text-align: center;">
+		<h3 style="margin-bottom: 5px; color:var(--feedbot-title);">
+			<?=ENJOY;?> <?=WEBSITE_NAME;?> ?
+		</h3>
+		<p style="word-break: break-word; white-space: break-spaces;"><?=MAKE_IT_KNOWN;?>.</p>
 
-    <div style="margin-bottom: 20px; text-align: center;">
-    <h3 style="margin-bottom: 5px; color:var(--feedbot-title);"><?=ENJOY;?> <?=WEBSITE_NAME;?> ?</h3>
-    <p style="word-break: break-word; white-space: break-spaces;"><?=MAKE_IT_KNOWN;?>.</p>
-
-    <div style="text-align: center; margin-top: 20px;">
-        <?php
-        $title = WEBSITE_NAME;
-        $description_link = TALK_ON_MASTODON_MESSAGE;
-        ?>
-        <form class="share_3382">
-            <input type="hidden" name="article_id" value="3382">
-            <input type="hidden" name="feed_id" value="289">
-            <input type="hidden" name="site_id" value="100">
-            <input type="hidden" name="messagetitle" value="<?=$title;?>">
-            <input type="hidden" name="message" value="<?=$description_link;?>">
-            <input type="hidden" name="url" value="https://feedbot.net">
-            <input type="hidden" name="peertubeid" value="">
-            <input type="hidden" name="youtubeid" value="">
-            <input type="hidden" name="thumbnail" value="https://feedbot.net/assets/icons/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png">
-            <button onclick="share(3382)" title="<?= SHARE ?>"><i class="fa fa-paper-plane fa-2x" aria-hidden="true" style="margin-right: 10px; vertical-align: middle;"></i> <?=TALK_ON_MASTODON;?> !</button>
-        </form>
-    </div>
-
-    </div>
+		<div style="text-align: center; margin-top: 20px;">
+			<form class="share_3382">
+				<input type="hidden" name="article_id" value="3382">
+				<input type="hidden" name="feed_id" value="289">
+				<input type="hidden" name="site_id" value="100">
+				<input type="hidden" name="messagetitle" value="<?=WEBSITE_NAME;?>">
+				<input type="hidden" name="message" value="<?=TALK_ON_MASTODON_MESSAGE;?>">
+				<input type="hidden" name="url" value="https://feedbot.net">
+				<input type="hidden" name="peertubeid" value="">
+				<input type="hidden" name="youtubeid" value="">
+				<input type="hidden" name="thumbnail" value="https://feedbot.net/assets/icons/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png">
+				<button onclick="share(3382)" title="<?= SHARE ?>">
+					<i class="fa fa-paper-plane fa-2x" aria-hidden="true" style="margin-right: 10px; vertical-align: middle;"></i> <?=TALK_ON_MASTODON;?> !
+				</button>
+			</form>
+		</div>
+	</div>
 </div>
