a71eb185509c3492b4e748bbb72476aeb1416743
Raph <49836486+raphgilles@users.noreply.github.com>
2023-03-18 20:05:59 +0100

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+$feed_id = $_POST['feed_id'];
+$uid = $_SESSION['uid'];
+
+include('../config.php');
+
+$chat_id = $_GET['id'];
+echo "Utilisateur : ".$uid."<br />";
+echo "chat_id : ".$chat_id."<br />";
+
+$sql = "UPDATE users SET telegram = ('$chat_id') WHERE id = '$uid'";
+mysqli_query($conn, $sql);
+
+if ($chat_id !== "") {
+header('Location: '.WEBSITE_URL.'/?p=settings&telegram=success');
+}
+else {
+header('Location: '.WEBSITE_URL.'/?p=settings&telegram=failed');
+}
+?>
